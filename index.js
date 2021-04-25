bc6decdeb7ef0a302d6d1bc898d169753903bd84
blangwell <b.langwell@outlook.com>
2021-04-25 01:05:14 -0700

    Add username functionality

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
+let users = [];
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
@@ -12,16 +13,29 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
   console.log("A user has connected!");
-  io.emit('chat notification', "A user has connected")
+  io.emit('chat notification', "A user has connected");
+
   socket.on('disconnect', () => {
     console.log("User Disconnected");
   });
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg)
-    console.log(`message: ${msg}`)
+ 
+  socket.on('chat message', data => {
+    io.emit('chat message', data);
   });
+
+  socket.on('set username', function(username) {
+    if (users.indexOf(username) === -1) {
+      users.push(username);
+      console.log('server users ' + users);
+      console.log('username var ' + username);
+      socket.emit('userSet', {username})
+    } else {
+      socket.emit('userExists', username + " username is taken!")
+    }
+  });
+
   socket.on('chat notification', msg => {
-    io.emit('chat notification', msg)
+    io.emit('chat notification', msg);
   })
 });
 
